48b6a1a984edd60e0db9d71267606545c95d987f
Manoj <manojlds@gmail.com>
2014-08-23 20:50:02 +0530

    Remove unused field

--- src/pslinq/Cmdlets/RepeatList.cs
+++ src/pslinq/Cmdlets/RepeatList.cs
@@ -5,18 +5,11 @@ namespace pslinq.Cmdlets
     [Cmdlet("Repeat", "List")]
     public class RepeatList : Cmdlet
     {
-        private int _noOfItemsDone;
-
         [Parameter(Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
         public object Input { get; set; }
 
         [Parameter(Position = 0, Mandatory = true)]
         public int Count { get; set; }
-
-        protected override void BeginProcessing()
-        {
-            _noOfItemsDone = 0;
-        }
 
         protected override void ProcessRecord()
         {
